e95636c378842c71122d98f304e7b54e2ac2bb5f
Bartek <gothosfolly@protonmail.com>
2020-02-03 13:55:59 +0100

    progress

--- SeleniumProject_1/src/automationFramework/LogInTest.java
+++ SeleniumProject_1/src/automationFramework/LogInTest.java
@@ -15,20 +15,25 @@ public class LogInTest {
 	static void test(WebDriver driver) throws InterruptedException {
 		
 		WebDriverWait wait = new WebDriverWait(driver, 60);
-		WebElement loginButton = driver.findElement(By.cssSelector("ul.marketing-nav__items:nth-child(4) > li:nth-child(3) > form:nth-child(1) > input:nth-child(1)"));
-		loginButton.click();
+		WebElement frontEndHomepage = driver.findElement(By.cssSelector("div.fadeInUp:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > a:nth-child(1)"));
+		frontEndHomepage.click();
 		
-		wait.until(ExpectedConditions.elementToBeClickable((driver.findElement(By.cssSelector(".recaptcha-checkbox-border")))));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"dropdownCurrency\"]")));
 
-		WebElement usernameBox = driver.findElement(By.cssSelector("#inputEmail"));
-		WebElement passwordBox = driver.findElement(By.cssSelector("#inputPassword"));
-		WebElement captchaBox = driver.findElement(By.cssSelector(".recaptcha-checkbox-border"));
-		WebElement loginButton2 = driver.findElement(By.cssSelector("#login"));
+		WebElement myAccountButton = driver.findElement(By.cssSelector(".dropdown-login > a:nth-child(1)"));
+		myAccountButton.click();
+		WebElement goToLoginButton = driver.findElement(By.cssSelector("a.tr:nth-child(1)"));
+		goToLoginButton.click();
+		
+		wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div.form-group:nth-child(1) > label:nth-child(1) > span:nth-child(2)")));
+		
+		WebElement usernameBox = driver.findElement(By.cssSelector("div.form-group:nth-child(1) > label:nth-child(1) > span:nth-child(2)"));
+		WebElement passwordBox = driver.findElement(By.cssSelector("div.wow:nth-child(3) > div:nth-child(2) > label:nth-child(1) > span:nth-child(2)"));
+		WebElement loginButton = driver.findElement(By.cssSelector("button.btn:nth-child(4)"));
 		
 		usernameBox.sendKeys("user@phptravels.com");
 		passwordBox.sendKeys("demouser");
-		captchaBox.click();
-		loginButton2.click();
+		loginButton.click();
 		
 	}
 
