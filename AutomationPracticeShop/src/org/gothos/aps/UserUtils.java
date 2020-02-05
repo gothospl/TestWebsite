678d09669cd6ba05164fe95351c4678362e1006a
Bartek <gothosfolly@protonmail.com>
2020-02-05 17:56:04 +0100

    progress

--- AutomationPracticeShop/src/org/gothos/aps/UserUtils.java
+++ AutomationPracticeShop/src/org/gothos/aps/UserUtils.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.Select;
 
 public class UserUtils {
 	
@@ -12,7 +13,7 @@ public class UserUtils {
 		throw new IllegalStateException("Utility class");
 	}
 	
-	static void addUser(WebDriver driver, String username, String password, String address) {
+	static void addUser(WebDriver driver, String username, String password, String address, String[] customerData ) {
 		
 		WebDriverWait wait = new WebDriverWait(driver, 60);
 		
@@ -35,26 +36,53 @@ public class UserUtils {
 		WebElement customerLastNameField = driver.findElement(By.cssSelector("customer_lastname"));
 		WebElement customerEmailAddressField = driver.findElement(By.cssSelector("#email"));
 		WebElement customerPasswordField = driver.findElement(By.cssSelector("#passwd"));
-		WebElement customerDOB_DayMenu = driver.findElement(By.cssSelector("#days"));
-		WebElement customerDOB_MonthMenu = driver.findElement(By.cssSelector("#months"));
-		WebElement customerDOB_YearMenu = driver.findElement(By.cssSelector("#years"));
+		Select customerDOB_DayMenu = new Select (driver.findElement(By.cssSelector("#days")));
+		Select customerDOB_MonthMenu = new Select (driver.findElement(By.cssSelector("#months")));
+		Select customerDOB_YearMenu = new Select(driver.findElement(By.cssSelector("#years")));
 		WebElement newsletterCheckbox = driver.findElement(By.cssSelector("#newsletter"));
-		WebElement optInCheckbox = driver.findElement(By.cssSelector("#optin"));
-		
+		WebElement optInCheckbox = driver.findElement(By.cssSelector("#optin"));		
 		WebElement addressFirstNameField = driver.findElement(By.cssSelector("#firstname"));
 		WebElement addressLastNameField = driver.findElement(By.cssSelector("#lastname"));
 		WebElement addressCompanyField = driver.findElement(By.cssSelector("#company"));
 		WebElement addressAddress1Field = driver.findElement(By.cssSelector("#address1"));
 		WebElement addressAddress2Field = driver.findElement(By.cssSelector("#address2"));
 		WebElement addressCityField = driver.findElement(By.cssSelector("#city"));
-		WebElement addressStateMenu = driver.findElement(By.cssSelector("#id_state"));
+		Select addressStateMenu = new Select(driver.findElement(By.cssSelector("#id_state")));
 		WebElement addressZipCodeField = driver.findElement(By.cssSelector("#postcode"));
-		WebElement addressCountryMenu = driver.findElement(By.cssSelector("#id_country"));
+		Select addressCountryMenu = new Select (driver.findElement(By.cssSelector("#id_country")));
 		WebElement addressAdditionalField = driver.findElement(By.cssSelector("#other"));
 		WebElement addressHomePhoneField = driver.findElement(By.cssSelector("#phone"));
 		WebElement addressMobilePhoneField = driver.findElement(By.cssSelector("#phone_mobile"));
 		WebElement addressAliasField = driver.findElement(By.cssSelector("#alias"));
 		WebElement registerButton = driver.findElement(By.cssSelector("#submitAccount"));
+		
+		if(gender == "Male") {
+			userTitleMr.click();
+		} else {
+			userTitleMrs.click();
+		}
+		
+		customerFirstNameField.sendKeys(firstname);
+		customerLastNameField.sendKeys(lastname);
+		customerEmailAddressField.sendKeys(username);
+		customerPasswordField.sendKeys(password);
+		customerDOB_DayMenu.selectByVisibleText(dobday);
+		customerDOB_MonthMenu.selectByVisibleText(dobmonth);
+		customerDOB_YearMenu.selectByVisibleText(dobyear);
+		addressFirstNameField.sendKeys(firstname);
+		addressLastNameField.sendKeys(lastname);
+		addressCompanyField.sendKeys(companyname);
+		addressAddress1Field.sendKeys(address1);
+		addressAddress2Field.sendKeys(address2);
+		addressCityField.sendKeys(city);
+		addressStateMenu.selectByVisibleText(state);
+		addressZipCodeField.sendKeys(zipcode);
+		addressCountryMenu.deselectByVisibleText(country);
+		addressAdditionalField.sendKeys(additional);
+		addressHomePhoneField.sendKeys(homephone);
+		addressMobilePhoneField.sendKeys(mobilephone);
+		addressAliasField.sendKeys(alias);
+		registerButton.click();
 		
 	}
 
